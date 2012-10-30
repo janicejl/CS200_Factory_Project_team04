523526d1babf90bc7336175683113604f3b409f7
Conrad Kurth <conradkurth@gmail.com>
2012-10-29 20:11:25 -0700

    Added kitting cell conveyor

---
+++ kittincellprojectv0/src/agents/kitrobotagents/KitConveyerAgent.java
@@ -0,0 +1,13 @@
+package Agents.KitRobotAgents;
+
+import Agent.Agent;
+
+public class KitConveyerAgent extends Agent{
+
+	@Override
+	protected boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}

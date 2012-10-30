b76a1751efbf0e6b2a41a52c7388334778d43816
Conrad Kurth <conradkurth@gmail.com>
2012-10-29 20:05:22 -0700

    Adding more agent files

---
+++ KittinCellProjectV0/src/Agents/KitRobotAgents/KitRobotAgent.java
@@ -0,0 +1,22 @@
+package Agents.KitRobotAgents;
+
+import Agent.Agent;
+
+public class KitRobotAgent extends Agent{
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	protected boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}

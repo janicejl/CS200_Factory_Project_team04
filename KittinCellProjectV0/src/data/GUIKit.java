1b1905ab76c42f9497c8d2cb380f90cab8f00982
Janice Leung <jjw1707@gmail.com>
2012-11-02 17:40:47 -0700

    removed useless debugging statements and comments

--- KittinCellProjectV0/src/data/GUIKit.java
+++ KittinCellProjectV0/src/data/GUIKit.java
@@ -29,7 +29,6 @@ public class GUIKit {
 		g.drawImage(image, (int)x, (int)y, null);
 		
 		Vector<Part> parts = kit.parts;
-		//Vector<BufferedImage> gParts = new Vector<BufferedImage>(); 
 		//Setting the position of each individual part. 
 		for (int i = 0, j = 0; i < parts.size(); i++) {
 			parts.get(i).setX(x + ((i % 2) * 32));		//32 = size of testing image parts. 
@@ -43,7 +42,6 @@ public class GUIKit {
 				p = ImageIO.read(new File("src/data/graphics/" + parts.get(i).getID() + ".png"));
 				g.drawImage(p, (int)parts.get(i).getX(), (int)parts.get(i).getY(), null);
 			} catch (IOException e) {
-				System.out.println(System.getProperty("user.dir"));
 				e.printStackTrace();
 			}
 			

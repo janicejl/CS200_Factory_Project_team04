e2b5d3a29378c37b19673922a576f872cb762ce2
Vincent <jianliac@usc.edu>
2012-11-08 10:08:05 -0800

    Part manager updata

---
+++ KittinCellProjectV0/src/PartsManager/PartsManagerApp.java
@@ -0,0 +1,12 @@
+package PartsManager;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class PartsManagerApp extends JFrame{
+	
+	public static void main(String[] args){
+		
+	}
+}
--- KittinCellProjectV0/src/PartsManager/PartsPanel.java
+++ KittinCellProjectV0/src/PartsManager/PartsPanel.java
@@ -2,11 +2,21 @@ package PartsManager;
 
 import java.awt.*;
 import java.awt.event.*;
-
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class PartsPanel extends JPanel implements ActionListener{
-
+	
+	PartsManagerApp app;
+	JComboBox imagesSelectBox;
+	ArrayList<JButton> removeButtons;
+	JButton removeAllButton, clearButton, createButton;
+	JTextField nameField;
+	
+	public PartsPanel(PartsManagerApp _app){
+		app = _app;
+		
+	}
 	public void actionPerformed(ActionEvent e){
 		
 	}

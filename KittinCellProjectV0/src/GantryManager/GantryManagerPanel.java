6f58969021ec3b4f47b4ad995825f550fb8e50d7
Vincent <jianliac@usc.edu>
2012-11-12 10:58:34 -0800

    add laneManager panel

---
+++ KittinCellProjectV0/src/GantryManager/GantryManagerPanel.java
@@ -0,0 +1,15 @@
+package GantryManager;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class GantryManagerPanel extends JPanel implements ActionListener{
+	JPanel feederPanel, GantryPanel;
+	ArrayList<JPanel> horizPanels;
+	
+	public void actionPerformed(ActionEvent ae){
+		
+	}
+}

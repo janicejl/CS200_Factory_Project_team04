74af8ee5e274292bb9ce2c60668464f79f67601d
Quentin <qhsu@usc.edu>
2012-11-11 18:52:18 -0800

    Kit creation backend update

--- KittinCellProjectV0/src/KitCreationManager/GUIKitModification.java
+++ KittinCellProjectV0/src/KitCreationManager/GUIKitModification.java
@@ -7,10 +7,12 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.Vector;
 import java.util.Vector;
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -19,17 +21,20 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import data.Kit;
+import data.PartInfo;
 
 public class GUIKitModification implements ActionListener{
+	KitCreationApp app;
 	JPanel base,partDisplay,up,back;
 	//JScrollPane displayKit;
 	JButton confirm;
 	JComboBox kitList;
-	Vector kitName;
-	ArrayList<String> m;
-	ArrayList<JButton> partList;
+	Vector<String> kitNames;
+	Vector<JButton> partList;
+	TreeMap<Integer, Boolean> tempDeleted;
 	JCheckBox remove;
-	public GUIKitModification(){
+	public GUIKitModification(KitCreationApp _app){
+		app = _app;
 		base=new JPanel();
 		base.setSize(300,370);
 		base.setBackground(Color.orange);
@@ -37,13 +42,21 @@ public class GUIKitModification implements ActionListener{
 		remove=new JCheckBox("remove");
 		remove.setOpaque(false);
 		confirm=new JButton("confirm");
-		partList=new ArrayList<JButton>();
+		confirm.addActionListener(this);
+		partList=new Vector<JButton>();
+		tempDeleted = new TreeMap<Integer, Boolean>();
+		for(int i = 0; i < 8; i++){
+			tempDeleted.put(i, false);
+		}
 		
-		kitName=new Vector();
-		kitName.add("");
-		kitName.add("Kit XXX");
-		m=new ArrayList<String>();
-		kitList=new JComboBox(kitName);
+		kitNames=new Vector<String>();
+		if(app.getKitsList().size() == 0){
+			kitNames.add("");
+		}
+		for(int i = 0; i < app.getKitsList().size(); i++){
+			kitNames.add(app.getKitsList().get(i).getName());
+		}
+		kitList=new JComboBox(kitNames);
 		kitList.addActionListener(this);
 		partDisplay.setOpaque(false);
 		up=new JPanel();
@@ -98,29 +111,43 @@ public class GUIKitModification implements ActionListener{
 	public void actionPerformed(ActionEvent ae) {
 		
 		if(ae.getSource().equals(kitList)){
-			if(kitList.getSelectedIndex()==1){
-				GridBagConstraints c=new GridBagConstraints();
-				c.gridx=0;
-				c.gridy=0;
-			
-				for(int i=0;i<8;i++){
-					JButton temp=new JButton(""+i);
-					temp.setPreferredSize(new Dimension (170,30));
-					temp.addActionListener(this);
-					partList.add(temp);
-					partDisplay.add(partList.get(i),c);
-					c.gridy+=2;
+		
+			GridBagConstraints c=new GridBagConstraints();
+			c.gridx=0;
+			c.gridy=0;
+		
+			for(int i=0;i<8;i++){
+				JButton temp = new JButton(new ImageIcon(app.getKitsList().get(kitList.getSelectedIndex()).getParts().get(i).getImagePath()));
+				temp.setPreferredSize(new Dimension (170,30));
+				temp.addActionListener(this);
+				partList.add(temp);
+				partDisplay.add(partList.get(i),c);
+				c.gridy+=2;
+			}
+		}
+		else if(ae.getSource().equals(confirm)){
+			for(int i = 0; i < tempDeleted.size(); i++){
+				if(tempDeleted.get(i) == true){
+					app.getKitsList().get(kitList.getSelectedIndex()).getParts().set(i, new PartInfo(null, "images/none.png"));
+					tempDeleted.put(i, false);
 				}
 			}
 		}
 		for(int i=0;i<partList.size();i++){
 			if(ae.getSource().equals(partList.get(i))){
 				if(remove.isSelected()){
-					partDisplay.remove(partList.get(i));
-					partList.remove(i);
+					if(tempDeleted.get(i) == false){
+						tempDeleted.put(i, true);
+						partList.get(i).setIcon(new ImageIcon("images/none.png"));
+					}
+					else if(tempDeleted.get(i) == true){
+						tempDeleted.put(i, false);
+						partList.get(i).setIcon(new ImageIcon(app.getKitsList().get(kitList.getSelectedIndex()).getParts().get(i).getImagePath()));
+					}
 				}
 			}
 		}
+		
 		partDisplay.revalidate();
 		partDisplay.repaint();
 		base.revalidate();

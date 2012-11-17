cf215274b69d3dca3f7dd59790454f78d37a1dea
Devin Dunn <devin.t.dunn@gmail.com>
2012-11-16 21:36:12 -0800

    Fixed some conflict bugs

--- KittinCellProjectV0/src/Agents/VisionAgent/VisionAgent.java
+++ KittinCellProjectV0/src/Agents/VisionAgent/VisionAgent.java
@@ -22,11 +22,9 @@ public class VisionAgent extends Agent implements Vision {
 	/** DATA **/
 	
 	String name;
-	List<NestAgent> nestsList= Collections.synchronizedList( new ArrayList<NestAgent>() ); // list of all the nests that we need parts from
 	List<Nest> nestsList= Collections.synchronizedList( new ArrayList<Nest>() ); // list of all the nests that we need parts from
 	List<Part> neededPartsList = Collections.synchronizedList( new ArrayList<Part>() ); // list of all the parts that should be in each kit
 	
-	Map<Integer, NestAgent> fullNestsMap = Collections.synchronizedMap( new TreeMap<Integer, NestAgent>() ); // list of all the nests that are full and ready to have their picture taken
 	Map<Integer, Nest> fullNestsMap = Collections.synchronizedMap( new TreeMap<Integer, Nest>() ); // list of all the nests that are full and ready to have their picture taken
 	List<Part.PartType> fullNestsPartsList = Collections.synchronizedList( new ArrayList<Part.PartType>() ); // parallel array that holds all parts that each nest is supposed to hold
 	
@@ -69,7 +67,6 @@ public class VisionAgent extends Agent implements Vision {
 	/////////////////////////////////////////////////////////////
 	/** MESSAGES **/
 	
-	public void msgHereIsSchematic(List<Part> partsList, List<NestAgent> nestsList)  {
 	public void msgHereIsSchematic(List<Part> partsList, List<Nest> nestsList)  {
 		// receive a list of all the parts that a kit needs, and a list of all the nests
 		this.nestsList = nestsList;
@@ -84,7 +81,6 @@ public class VisionAgent extends Agent implements Vision {
 	}
 	
 	// sent by NestAgent
-	public void msgImFull(NestAgent nest) {
 	public void msgImFull(Nest nest) {
 		// here i am assuming that we will name the nests with a number.
 		// i can do this differently if we later decide not to do this, but this makes it easier for me to organize parts

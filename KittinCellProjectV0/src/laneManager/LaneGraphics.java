8f3668404d45575ddea21b256d4aba180bdfeef6
Quentin <qhsu@usc.edu>
2012-11-18 02:10:06 -0800

    fixed lane graphics

--- KittinCellProjectV0/src/laneManager/LaneGraphics.java
+++ KittinCellProjectV0/src/laneManager/LaneGraphics.java
@@ -191,7 +191,7 @@ public class LaneGraphics extends JPanel implements ActionListener {
     }
     
     public void releaseItem(int lane) {
-    	lanes.get(lane).releasePart(lane);
+    	lanes.get(lane).releasePart();
     }
     
     public void removeItem(int lane){

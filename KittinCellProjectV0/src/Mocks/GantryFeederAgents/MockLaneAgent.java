ed82cd8255772b803173e995ada4d6bdb4fff2ba
Sara Gramling <sara.gramling@gmail.com>
2012-11-17 21:59:19 -0800

    Set up for unit testing on FeederAgent

---
+++ KittinCellProjectV0/src/Mocks/GantryFeederAgents/MockLaneAgent.java
@@ -0,0 +1,55 @@
+package Mocks.GantryFeederAgents;
+import data.Part;
+import data.Part.PartType;
+import Interface.PartsRobotAgent.*;
+import Mocks.KitRobotAgents.MockAgent;
+import UnitTest.KitRobotAgents.*;
+
+public class MockLaneAgent extends MockAgent implements Lane{
+
+	public EventLog log = new EventLog();
+	int index;
+	
+	public MockLaneAgent(String name, int index) {
+		super(name);
+		this.index = index;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void msgReadyForPart() {
+		log.add(new LoggedEvent("Nest Ready for Part"));
+	}
+
+	@Override
+	public void msgNeedThisPart(PartType type) {
+		log.add(new LoggedEvent("Nest Wants Part"));
+		
+	}
+
+	@Override
+	public void msgPartAtEndOfLane() {
+		log.add(new LoggedEvent("Part at End of Lane"));
+		
+	}
+
+	@Override
+	public void msgHereIsAPart(Part p) {
+		// TODO Auto-generated method stub
+		log.add(new LoggedEvent("msgHereIsAPart received from Feeder."));
+	}
+
+	@Override
+	public void msgCanIPlacePart() {
+		// TODO Auto-generated method stub
+		log.add(new LoggedEvent("msgCanIPlacePart received from Feeder"));
+	}
+
+	@Override
+	public int getNumber() {
+		// TODO Auto-generated method stub
+		log.add(new LoggedEvent("getNumber() called"));
+		return index;
+	}
+
+}
--- KittinCellProjectV0/src/UnitTest/GantryFeederAgents/FeederTests.java
+++ KittinCellProjectV0/src/UnitTest/GantryFeederAgents/FeederTests.java
@@ -1,11 +1,34 @@
 package UnitTest.GantryFeederAgents;
 
 import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
 import org.junit.Test;
 
-public class FeederTests {
+import server.Server;
 
+import Agents.GantryFeederAgents.FeederAgent;
+import Mocks.GantryFeederAgents.MockGantryAgent;
+import Mocks.GantryFeederAgents.MockGantryControllerAgent;
+import Mocks.GantryFeederAgents.MockLaneAgent;
+
+public class FeederTests extends TestCase{
+
+	public FeederAgent feeder1;
+	public MockLaneAgent lane1;
+	public MockLaneAgent lane2;
+	public MockGantryControllerAgent gc;
+	public MockGantryAgent gantry1;
+	
+	protected void setUp() throws Exception{
+		super.setUp();
+		lane1 = new MockLaneAgent("left", 0);
+		lane2 = new MockLaneAgent("right", 1);
+		feeder1 = new FeederAgent("Feeder 1", 5, lane1, lane2, 0, new Server() );
+		gc = new MockGantryControllerAgent("gc");
+		gantry1 = new MockGantryAgent("Gantry 1");
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");

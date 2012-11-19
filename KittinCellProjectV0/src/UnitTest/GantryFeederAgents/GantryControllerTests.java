a14bc3faabfc6ed3cf4c325bf3542fda7d7b1a71
Sara Gramling <sara.gramling@gmail.com>
2012-11-18 23:34:38 -0800

    More Gantry Controller Unit Testing

--- KittinCellProjectV0/src/UnitTest/GantryFeederAgents/GantryControllerTests.java
+++ KittinCellProjectV0/src/UnitTest/GantryFeederAgents/GantryControllerTests.java
@@ -22,8 +22,8 @@ public class GantryControllerTests extends TestCase {
 
 	GantryControllerAgent gc;
 	MockGantryAgent gantry1;
-	MockGantryAgent gantry2;
 	MockFeederAgent feeder1;
+	MockFeederAgent feeder2;
 	Bin bin1;
 	Bin bin2;
 	PartInfo type1;
@@ -37,6 +37,8 @@ public class GantryControllerTests extends TestCase {
 		super.setUp();
 		gc = new GantryControllerAgent(new Server());
 		gantry1 = new MockGantryAgent("Gantry 1");
+		feeder1 = new MockFeederAgent("Feeder 1", 0);
+		feeder2 = new MockFeederAgent("Feeder 2", 1);
 		type1 = new PartInfo("Part 1", "source 1");
 		type2 = new PartInfo("Part 2", "source 2");
 		bin1 = new Bin(type1, 20);
@@ -64,5 +66,42 @@ public class GantryControllerTests extends TestCase {
 		
 		
 	}
+	
+	@Test
+	public void testMultipleFeederInteractions() {
+		
+		gc.msgBinConfiguration(bins);
+		assertTrue("Gantry controller should have received msgBinConfiguration from FCS", 
+				gc.log.containsString("msgBinConfiguration"));
+		
+		gc.msgNeedThisPart(type1, feeder1);
+		assertTrue("Gantry controller should have received msgNeedThisPart from Feeder 1", 
+				gc.log.containsString("msgNeedThisPart"));
+		
+		gc.log.clear();
+		
+		gc.msgNeedThisPart(type2, feeder2);
+		assertTrue("Gantry controller should have received msgNeedThisPart from Feeder 2", 
+				gc.log.containsString("msgNeedThisPart"));
+		
+		while(gc.pickAndExecuteAnAction());		
+		//gc.pickAndExecuteAnAction();
+		assertTrue("Gantry1 should have received msgGiveFeederParts from GC", gantry1.log.containsString("msgGiveFeederParts"));
+		
+		gc.msgDoneDeliveringParts(gantry1);
+		assertTrue("Gantry Controller should have received msgDoneDeliveringParts from Gantry 1", gc.log.containsString("msgDoneDeliveringParts"));
+		
+		gc.log.clear();
+		while(gc.pickAndExecuteAnAction());		
+		//gc.pickAndExecuteAnAction();
+		assertTrue("Gantry1 should have received msgGiveFeederParts from GC", gantry1.log.containsString("msgGiveFeederParts"));
+		
+		gc.msgDoneDeliveringParts(gantry1);
+		assertTrue("Gantry Controller should have received msgDoneDeliveringParts from Gantry 1", gc.log.containsString("msgDoneDeliveringParts"));
+		
+		
+	}
+	
+	
 
 }

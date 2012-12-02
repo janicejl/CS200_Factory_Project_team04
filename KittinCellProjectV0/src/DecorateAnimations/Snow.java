30f7794dfaf7cc3b91b9d1349f1d706e3a910d5d
anpinshe <anpinshe@usc.edu>
2012-12-01 16:22:27 -0800

    snowy background, but wonder why not come up :)

---
+++ KittinCellProjectV0/src/DecorateAnimations/Snow.java
@@ -0,0 +1,64 @@
+package DecorateAnimations;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+public class Snow{
+	BufferedImage snowflake;
+	double x;
+	double y;
+	boolean goLeft;
+	public Snow() {
+		try {
+			snowflake = ImageIO.read(new File("images/snow.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		double r=Math.random()*1200;
+		x=r;
+		y=0;
+	}
+
+	public synchronized Image getSnowflake() {
+		return snowflake;
+	}
+
+	public synchronized void setSnowflake(BufferedImage snowflake) {
+		this.snowflake = snowflake;
+	}
+
+	public synchronized double getX() {
+		return x;
+	}
+
+	public synchronized void setX(double x) {
+		this.x = x;
+	}
+
+	public synchronized double getY() {
+		return y;
+	}
+
+	public synchronized void setY(double y) {
+		this.y = y;
+	}
+
+	public synchronized boolean isLeft() {
+		return goLeft;
+	}
+
+	public synchronized void setLeft(boolean leftOrRight) {
+		this.goLeft = leftOrRight;
+	}
+
+
+
+}
+

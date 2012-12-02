fc510ff6f03569f94ebdd73243f625a69a082909
Sara Gramling <sara.gramling@gmail.com>
2012-12-01 19:07:52 -0800

    updated Gantry/Feeder interfaces

--- KittinCellProjectV0/src/Interface/GantryFeederAgent/Feeder.java
+++ KittinCellProjectV0/src/Interface/GantryFeederAgent/Feeder.java
@@ -7,31 +7,11 @@ import data.PartInfo;
 
 public interface Feeder {
 	
-	
-	
-	
 	public void msgNeedThisPart(PartInfo p, Lane lane);
 	public void msgHereAreParts(Bin bin);
 	public void msgAmIReadyForParts();
 	public void msgPartsGone();
 	public void msgIsLaneReadyForParts(Lane lane_);
 	public int getNumber();
-	//public void msgLaneIsFull(Lane lane_);
-
-
-
-	/*void msgNeedThisPart(PartInfo p, Lane lane);
-	void msgHaveParts(Gantry g1);
-	void msgHereAreParts(Bin b);
-	void msgHereAreParts(PartInfo p, int quantity);
-	void msgLaneIsFull(String laneName);
-	void msgLaneIsReadyForParts(String laneName);
-	//void msgLaneAnimationDone();
-	//void msgFeederAnimationDone();
 	
-	
-	void setGantry(Gantry g1);
-	void removeGantry();
-	String getName();
-	int getNumber();*/
 }
